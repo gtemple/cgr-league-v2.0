ac02ccaa5ccf6dfd81261b493f5c1f3e1705d719
Giordano Temple <giordanotemple@gmail.com>
2024-07-14 10:45:55 +0200

    finish hamburger functionality

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -7,14 +7,21 @@ import {
   MenuSeparator,
 } from "@headlessui/react";
 import useGetHumanDrivers from "../../const/hooks/useGetHumanDrivers";
+import useWindowDimensions from "../../const/hooks/util/useWindoDimentions";
 
 const Navbar = () => {
   const [dark, setDark] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
+  const { width } = useWindowDimensions();
   const { humanDriversData } = useGetHumanDrivers();
 
   const darkModeHandler = () => {
     setDark(!dark);
     document.body.classList.toggle("dark");
+  };
+
+  const navOpenHandler = () => {
+    setNavOpen(!navOpen);
   };
 
   return (
@@ -23,77 +30,84 @@ const Navbar = () => {
     border-b border-solid"
     >
       <div className="mx-4">CGR League</div>
-      <button
-        data-collapse-toggle="navbar-hamburger"
-        type="button"
-        className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        aria-controls="navbar-hamburger"
-        aria-expanded="false"
-      >
-        <span className="sr-only">Open main menu</span>
-          button
-      </button>
-      <div className="md:flex-row flex-col w-full">
-        <div>
-        <Menu as='div' className='mx-4 flex-col'>
-          <MenuButton
-            className="p-2 rounded-md data-[active]:bg-neutral-800 uppercase hover:text-sky-300
+      {width < 768 && (
+        <button
+          onClick={() => navOpenHandler()}
+          className="absolute top-0 right-0 px-8 py-8"
+        >
+          Hamburger
+        </button>
+      )}
+
+      {navOpen ? (
+        <div
+          className="flex md:flex-row w-full flex-col md:static absolute
+                      bg-slate-300 md:bg-inherit
+                      md:m-0 m-3 
+                      z-2 md:inset-0 top-0
+      "
+        >
+          <button onClick={() => navOpenHandler()}>x</button>
+          <Menu>
+            <MenuButton
+              className="p-2 rounded-md data-[active]:bg-neutral-800 uppercase hover:text-sky-300
             data-[active]:outline-2 data-[active]:outline data-[active]:outline-slate-50 data-[active]:text-sky-300"
-          >
-            Drivers
-          </MenuButton>
-          <MenuItems
-            anchor="bottom"
-            className="bg-gradient-to-b from-neutral-800 to-neutral-600  text-sky-100 p-3 rounded-md mt-1 "
-          >
-            {humanDriversData.map((humanDriver) => (
-              <MenuItem key={humanDriver.id}>
-                <a
-                  className="block data-[focus]:bg-blue-100 data-[focus]:text-neutral-600 data-[focus]:px-auto data-[focus]:rounded-md p-2"
-                  href="/settings"
-                  key={humanDriver.id}
-                >
-                  {humanDriver.first_name} {humanDriver.last_name}
+            >
+              Drivers
+            </MenuButton>
+            <MenuItems
+              anchor="bottom"
+              transition
+              className="bg-gradient-to-b from-neutral-800 to-neutral-600  text-sky-100 p-3 rounded-md mt-1 "
+            >
+              {humanDriversData.map((humanDriver) => (
+                <MenuItem key={humanDriver.id}>
+                  <a
+                    className="block data-[focus]:bg-blue-100 data-[focus]:text-neutral-600 data-[focus]:px-auto data-[focus]:rounded-md p-2"
+                    href="/settings"
+                    key={humanDriver.id}
+                  >
+                    {humanDriver.first_name} {humanDriver.last_name}
+                  </a>
+                </MenuItem>
+              ))}
+              <MenuSeparator className="my-1 h-px bg-black" />
+              <MenuItem>
+                <a className="block data-[focus]:bg-blue-100 data-[focus]:text-neutral-600 data-[focus]:px-auto data-[focus]:rounded-md p-2">
+                  All Drivers
                 </a>
               </MenuItem>
-            ))}
-            <MenuSeparator className="my-1 h-px bg-black" />
-            <MenuItem>
-              <a className="block data-[focus]:bg-blue-100 data-[focus]:text-neutral-600 data-[focus]:px-auto data-[focus]:rounded-md p-2">
-                All Drivers
-              </a>
-            </MenuItem>
-          </MenuItems>
-        </Menu>
-        <Menu>
-          <MenuButton>Seasons</MenuButton>
-          <MenuItems anchor="bottom">
-            <MenuItem>
-              <a className="block data-[focus]:bg-blue-100 ">wip</a>
-            </MenuItem>
-          </MenuItems>
-        </Menu>
-        <Menu>
-          <MenuButton>Tracks</MenuButton>
-          <MenuItems anchor="bottom">
-            <MenuItem>
-              <a className="block data-[focus]:bg-blue-100">wip</a>
-            </MenuItem>
-          </MenuItems>
-        </Menu>
-        <Menu>
-          <MenuButton>Teams</MenuButton>
-          <MenuItems anchor="bottom">
-            <MenuItem>
-              <a className="block data-[focus]:bg-blue-100">wip</a>
-            </MenuItem>
-          </MenuItems>
-        </Menu>
-      </div>
-      <button onClick={() => darkModeHandler()}>
-        {dark ? "dark" : "light"}
-      </button>
-      </div>
+            </MenuItems>
+          </Menu>
+          <Menu>
+            <MenuButton>Seasons</MenuButton>
+            <MenuItems anchor="bottom">
+              <MenuItem>
+                <a className="block data-[focus]:bg-blue-100 ">wip</a>
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+          <Menu>
+            <MenuButton>Tracks</MenuButton>
+            <MenuItems anchor="bottom">
+              <MenuItem>
+                <a className="block data-[focus]:bg-blue-100">wip</a>
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+          <Menu>
+            <MenuButton>Teams</MenuButton>
+            <MenuItems anchor="bottom">
+              <MenuItem>
+                <a className="block data-[focus]:bg-blue-100">wip</a>
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+          <button onClick={() => darkModeHandler()}>
+            {dark ? "dark" : "light"}
+          </button>{" "}
+        </div>
+      ) : null}
     </div>
   );
 };
